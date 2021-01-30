1a6c24314fc40c49ff66f169b58ff0ac44695bca
Francesco95it <francesco.spagnolo95@gmail.com>
2021-01-30 19:19:24 +0100

    ES6 update for printer

--- src/utils/printer.js
+++ src/utils/printer.js
@@ -5,33 +5,33 @@ export function printWinner (playerScore) {
 	if (!playerScore) {
 		console.log("Tie.");
 	} else {
-		let compositeString = playerScore.player.name + " wins. - with " + constants.combinationsNames[playerScore.key];
+		let basicString = `${playerScore.player.name} wins. - with ${constants.combinationsNames[playerScore.key]}`;
 		switch (playerScore.key) {
 			case "highestCard": 
-				compositeString+=": " + constants.figureNames[playerScore.scoringCards.figure];
+				basicString = `${basicString} ${constants.figureNames[playerScore.scoringCards.figure]}`;
 				break;
 			case "pair": 
-				compositeString += " of " + constants.figureNames[playerScore.scoringCards[0][0].figure];
+				basicString = `${basicString} of ${constants.figureNames[playerScore.scoringCards[0][0].figure]}`;
 				break;
 			case "twoPairs": 
-				compositeString += " of " + constants.figureNames[playerScore.scoringCards[0][0].figure] + " and " + constants.figureNames[playerScore.scoringCards[1][0].figure];
+				basicString = `${basicString} of ${constants.figureNames[playerScore.scoringCards[0][0].figure]} and ${constants.figureNames[playerScore.scoringCards[1][0].figure]}`;
 				break;
 			case "tris": 
-				compositeString += " of " + constants.figureNames[playerScore.scoringCards[0].figure];
+				basicString = `${basicString} of ${constants.figureNames[playerScore.scoringCards[0].figure]}`;
 				break;
 			case "flush": 
-				compositeString += " of " + constants.suitNames[playerScore.scoringCards[0].suit];
+				basicString = `${basicString} of ${constants.suitNames[playerScore.scoringCards[0].suit]}`;
 				break;
 			case "fullHouse": 
-				compositeString += ": " + constants.figureNames[playerScore.scoringCards.tris[0].figure] + " over " + constants.figureNames[playerScore.scoringCards.pair[0].figure] ;
+				basicString = `${basicString}: ${constants.figureNames[playerScore.scoringCards.tris[0].figure]} over ${constants.figureNames[playerScore.scoringCards.pair[0].figure]}` ;
 				break;
 			case "poker": 
-				compositeString += " of " + constants.figureNames[playerScore.scoringCards[0].figure];
+				basicString = `${basicString} of ${constants.figureNames[playerScore.scoringCards[0].figure]}`;
 				break;
 			case "straightFlush": 
-				compositeString += " to " + constants.figureNames[playerScore.scoringCards[0].figure] + " of " + constants.suitNames[playerScore.scoringCards[0].suit];
+				basicString = `${basicString} to ${constants.figureNames[playerScore.scoringCards[0].figure]} of ${constants.suitNames[playerScore.scoringCards[0].suit]}`;
 				break;
 		}
-		console.log(compositeString);
+		console.log(basicString);
 	}
 }
